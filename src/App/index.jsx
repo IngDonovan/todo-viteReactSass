63d7c6e09dd2e4dc4fc111b5188e9346f56a09a3
Donovan Rojas <rojasdrm03@gmail.com>
2023-07-25 18:56:55 -0500

    first commit

---
+++ src/App/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { TodoHeader } from '../TodoHeader';
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <TodoHeader>
+        
+      </TodoHeader>
+    </>
+  )
+}
+
+export default App
---
+++ src/TodoHeader/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TodoHeader() {
+  return (
+    <header>
+      {/* {children} */}
+      <h1>header</h1>
+    </header>
+  );
+}
+
+export { TodoHeader };
