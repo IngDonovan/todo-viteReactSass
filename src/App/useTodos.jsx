26c2dcb09acb14595a9bde1fca37d1ba87ce6392
Donovan Rojas <rojasdrm03@gmail.com>
2023-07-25 19:08:35 -0500

    add two hooks

---
+++ src/App/useTodos.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { useLocalStorage } from "./useLocalStorage";
+
+function useTodos() {
+  //la lógica de varios niveles
+  const {
+    item: todos,
+    saveItem: saveTodos,
+    loading,
+    error,
+  } = useLocalStorage("TASK_V1", []);
+
+  const [searchValue, setSearchValue] = React.useState("");
+
+  const [openModal, setOpenModal] = React.useState(false);
+
+  const completedTodos = todos.filter((todo) => !!todo.completed).length; //doble negación para convertirlo si o si a boolean
+  console.log(completedTodos); // acá va bien cuenta los completados----------------
+  const totalTodos = todos.length;
+
+  const searchedTodos = todos.filter((todo) => {
+    const todoText = todo.text.toLowerCase();
+    const searchText = searchValue.toLocaleLowerCase();
+    return todoText.includes(searchText);
+  });
+
+  const addTodo = (text) => {
+    const newTodos = [...todos];
+    newTodos.push({
+      text,
+      completed: false,
+    });
+    saveTodos(newTodos);
+  };
+
+  const completeTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex((todo) => todo.text === text);
+    // newTodos[todoIndex].completed = true;
+    newTodos[todoIndex].completed = !newTodos[todoIndex].completed;
+    saveTodos(newTodos);
+  };
+
+  const deleteTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex((todo) => todo.text === text);
+    newTodos.splice(todoIndex, 1);
+    saveTodos(newTodos);
+  };
+
+  return {
+    loading,
+    error,
+    searchValue,
+    setSearchValue,
+    completedTodos,
+    totalTodos,
+    searchedTodos,
+    addTodo,
+    completeTodo,
+    deleteTodo,
+    openModal,
+    setOpenModal,
+  };
+}
+
+export { useTodos };
