71bd5a6f59f3e5c5bf104012ca1e10c08aee5ee0
Donovan Rojas <rojasdrm03@gmail.com>
2024-02-15 18:20:47 -0500

    add styles to new alert

--- src/ChangeAlert/index.jsx
+++ src/ChangeAlert/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { withStorageListener } from "./withStorageListener";
+import './ChangeAlert.scss';
 
 function ChangeAlert({ show, toggleShow }) {
     if (show) {
         return (
-            <div>
+            <div className="alertSync">
                 <p>Actualiza porque hubo cambios!</p>
                 <button
                     onClick={() => toggleShow(false)}
